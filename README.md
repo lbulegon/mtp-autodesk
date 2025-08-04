cac92d28d7a031453fae04e9a3fd690bc89d4a44
lbulegon <lbulegon@gmail.com>
2025-08-03 23:27:10 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+# Como rodar
+Instalar dependências:
+
+npm install
+
+Rodar em modo desenvolvimento:
 npm run dev
+
+Build para produção:
+npm run build
--- electron/preload.ts
+++ electron/preload.ts
@@ -1,5 +1,7 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld("api", {
-  ping: () => "pong"
+contextBridge.exposeInMainWorld('electronAPI', {
+  minimize: () => ipcRenderer.send('window-minimize'),
+  maximize: () => ipcRenderer.send('window-maximize'),
+  close: () => ipcRenderer.send('window-close'),
 });
